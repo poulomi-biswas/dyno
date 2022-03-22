59a2972380e13185c21827617440d4b6cafb071f
Priyangsu Banerjee <priyangsu26@gmail.com>
2022-03-22 23:42:51 +0530

    rebase

--- index.js
+++ index.js
@@ -4,19 +4,10 @@ const bodyParser = require("body-parser");
 const cors = require("cors");
 const port = process.env.PORT || 3000;
 const path = require("path");
-const sessionCode = require("./sessionCode");
 
 app.use(cors());
 app.use(bodyParser.json());
 app.use(express.static(path.join(__dirname, "public")));
-
-app.get("/session/:id", async (req, res) => {
-  var id = req.params.id;
-  var host = req.headers.host;
-  console.log(id, host);
-  var dyno = sessionCode(id, host);
-  res.status(200).send(dyno);
-});
 
 app.listen(port, () => {
   console.log(`Server is running on port: ${port}`);
